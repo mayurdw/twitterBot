45e6af98db8850a871f14c6d0a7bdf11c1b31c1a
mayurdw <mayurwadhwanid@gmail.com>
2019-12-08 21:31:59 +1300

    Feed file naming methods

--- C/main.c
+++ C/main.c
@@ -10,35 +10,54 @@
 #include <stdint.h>
 #include <unistd.h>
 #include <curl/curl.h>
+#include <string.h>
+#include <dirent.h>
+
+#define MAX_FILENAME_LEN    16
+#define BLOG_FEED_URL       "https://itsmayurremember.wordpress.com/feed"
+#define FILENAME_EXT        ".feed"
+#define CONFIG_FILENAME     "CONFIG"
+#define FILENAME_KEY        "FEEDFILE"
 
 typedef enum
 {
     NO_ERROR = 0,
+    INVALID_ARG,
 } ERROR_CODE;
 
 typedef struct 
 {
-  const char *pszFilename;
+  char szFileName[MAX_FILENAME_LEN+1];
   FILE *psStream;
 } RSS_FILE_STREAM;
- 
+
+static const char* s_pszFileFormat = "%04i%02i%02i" FILENAME_EXT;
+
+// Static Functions
+static ERROR_CODE GenerateFileName( char *pszFileName, uint32_t ulBufferSize );
+static size_t writeStreamToFile(void *pvBuffer, size_t iSize, size_t iNMemb, void *pvStream);
+static ERROR_CODE DownloadFeedFile( const char *pszURL );
+static bool FeedFileExists( void );
+
 static size_t writeStreamToFile(void *pvBuffer, size_t iSize, size_t iNMemb, void *pvStream)
 {
   RSS_FILE_STREAM *psOutStream = (RSS_FILE_STREAM *)pvStream;
   if(!psOutStream->psStream) {
     /* open file for writing */ 
-    psOutStream->psStream = fopen(psOutStream->pszFilename, "wb");
+    psOutStream->psStream = fopen(psOutStream->szFileName, "wb");
     if(!psOutStream->psStream)
       return -1; /* failure, can't open file to write */ 
   }
   return fwrite(pvBuffer, iSize, iNMemb, psOutStream->psStream);
 }
 
-ERROR_CODE DownloadFeedFile( const char *pszURL )
+static ERROR_CODE DownloadFeedFile( const char *pszURL )
 {
     CURLM *psCurl = NULL;
     CURLcode resCode = CURLE_OK;
-    RSS_FILE_STREAM sFileStream = { "Test.txt", NULL };
+    RSS_FILE_STREAM sFileStream = {0, };
+
+    GenerateFileName(sFileStream.szFileName, sizeof( sFileStream.szFileName));
 
     curl_global_init(CURL_GLOBAL_ALL);
     psCurl = curl_easy_init();
@@ -63,18 +82,52 @@ ERROR_CODE DownloadFeedFile( const char *pszURL )
     return NO_ERROR;
 }
 
-bool FeedFileExists( const char *pszFileName )
+static bool FeedFileExists( void )
 {
-    return false;
+    DIR *psDir = opendir("./");
+    bool bRet = false;
+
+    if( psDir != NULL )
+    {
+       struct dirent *psCurrentFile = NULL;
+
+       while( !bRet && ( psCurrentFile = readdir(psDir) ) != NULL )
+       {
+           bRet = ( strstr( psCurrentFile->d_name, FILENAME_EXT) != NULL);
+       }
+
+    }
+
+    return bRet;
 }
 
-ERROR_CODE GenerateFileName( char *pszFileName, uint32_t ulBufferSize )
+static ERROR_CODE GenerateFileName( char *pszFileName, uint32_t ulBufferSize )
+{
+    time_t sTime = {0,};
+    struct tm *psTimeNow = NULL; 
+    if( !pszFileName || ulBufferSize < MAX_FILENAME_LEN)
+    return INVALID_ARG;
+
+    memset(pszFileName, 0, ulBufferSize);
+    time(&sTime);
+    psTimeNow = localtime(&sTime);
+
+    snprintf(pszFileName, ulBufferSize, s_pszFileFormat, psTimeNow->tm_year + 1900, psTimeNow->tm_mon + 1, psTimeNow->tm_mday );
+
+    return NO_ERROR;
+}
+
+static ERROR_CODE SetLastFeedFileName( const char *pszFileName )
+{
+    return NO_ERROR;
+}
+
+static ERROR_CODE GetLastFeedFilename( char *pszFilename, uint32_t ulFilename)
 {
     return NO_ERROR;
 }
 
 int main( )
 {
-    DownloadFeedFile("https://itsmayurremember.wordpress.com/feed");
     return 0;
 }
